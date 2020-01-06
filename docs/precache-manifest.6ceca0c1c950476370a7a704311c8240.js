055d29fe9f929aefb67c7d3dcad5c6c734e99e15
mrprofessor <kar.rudra008@gmail.com>
2020-01-06 13:38:31 +0530

    Created docs folder to easily deploy to github

---
+++ docs/precache-manifest.6ceca0c1c950476370a7a704311c8240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49a2437eccd2141560f2221a1fefba1",
+    "url": "/solitude/index.html"
+  },
+  {
+    "revision": "a04e94c2e7195174d4e8",
+    "url": "/solitude/static/css/2.5b386a92.chunk.css"
+  },
+  {
+    "revision": "451c15dbffb1ee6d4e93",
+    "url": "/solitude/static/css/main.8525c558.chunk.css"
+  },
+  {
+    "revision": "a04e94c2e7195174d4e8",
+    "url": "/solitude/static/js/2.205afa04.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/solitude/static/js/2.205afa04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "451c15dbffb1ee6d4e93",
+    "url": "/solitude/static/js/main.2b56a285.chunk.js"
+  },
+  {
+    "revision": "80ac33450b467341a74b",
+    "url": "/solitude/static/js/runtime-main.d5ac4ae1.js"
+  }
+]);
